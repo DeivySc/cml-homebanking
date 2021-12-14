a19ffdfb8fd9680bd699b6c7a69ac6fa7850c06f
Deivy Jampool Sánchez Coronado <80715089+DeivySc@users.noreply.github.com>
2021-12-14 16:27:13 -0500

    avances responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Observable} from "rxjs";
+import {BreakpointObserver, Breakpoints, BreakpointState} from "@angular/cdk/layout";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cml-homebanking';
+  @ViewChild('drawer') drawer: any;
+  public selectedItem : string = '';
+  public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(map((result: BreakpointState) => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
+
+  closeSideNav() {
+    if (this.drawer._mode == 'over') {
+      this.drawer.close();
+    }
+  }
 }
